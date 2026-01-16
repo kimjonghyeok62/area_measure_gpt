6f8c52f299caaeeae3b481c88e406475d3f8ef51
Kim Jong Hyeok <106424172+kimjonghyeok62@users.noreply.github.com>
2026-01-16 09:36:10 +0900

    Updates

--- assets/index-A_hms89F.js
+++ assets/index-A_hms89F.js
@@ -27,6 +27,7 @@ body {
   background: var(--bg);
   color: var(--text);
   line-height: 1.5;
+  font-size: 18px;
 }
 
 .container {
@@ -40,7 +41,7 @@ body {
 }
 
 .h1 {
-  font-size: 1.75rem;
+  font-size: 2rem;
   font-weight: 800;
   margin: 0;
   word-break: keep-all;
@@ -48,7 +49,7 @@ body {
 
 .p {
   margin: 8px 0 0 0;
-  font-size: 1rem;
+  font-size: 1.125rem;
   color: var(--muted);
   line-height: 1.45;
   word-break: keep-all;
@@ -148,7 +149,7 @@ body {
   width: 100%;
   border-collapse: separate;
   border-spacing: 0;
-  min-width: 650px; /* 테이블 최소 너비 설정으로 모바일 스크롤 유도 */
+  min-width: 550px; /* 테이블 최소 너비 설정으로 모바일 스크롤 유도 */
 }
 
 .th {
@@ -172,7 +173,7 @@ body {
 
 .input {
   width: 100%;
-  max-width: 80px;
+  max-width: 60px;
   border: 1px solid var(--border);
   border-radius: 8px;
   padding: 8px 10px;
@@ -189,7 +190,7 @@ body {
 
 .readonly {
   width: 100%;
-  min-width: 80px;
+  min-width: 60px;
   text-align: right;
   border: 1px solid var(--border);
   border-radius: 8px;
